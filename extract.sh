812c0a0fa749265d8b9a032418f3d02eed7ea976
Nilo Pedrazzini <nilo.pedrazzini@gmail.com>
2021-10-16 17:36:37 +0100

    update shell scripts to include MDS/kriging

--- extract.sh
+++ extract.sh
@@ -1,21 +1,26 @@
 #!/bin/sh
 #Usage:
-#./preprocess.sh
+#./extract.sh
 #
 ################################################################
 
 #program path
 ROOT=./scripts
 extractoccurrence=$ROOT/extract_occur.py
+mdskriging=$ROOT/MDSandKriging.py
 
 source ./parallels-venv/bin/activate
 chmod a+rwx "$extractoccurrence"
+chmod a+rwx "$mdskriging"
 
 #__begin timer___
 START=$(date +%s)
 
 #---------------
 $extractoccurrence
+
+#---------------
+$mdskriging
 
 #___end timer___
 END=$(date +%s)
